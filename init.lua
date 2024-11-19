93a76ce8fdd98d1c2aa17b5a9b996477243b0d70
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-11-18 23:34:30 -0800

    search file names

--- init.lua
+++ init.lua
@@ -477,6 +477,7 @@ require('lazy').setup({
       -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'live grep' })
       vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, { desc = 'fuzzy search in current file' })
+      vim.keymap.set('n', '<leader>s', builtin.find_files, { desc = 'find files' })
       vim.keymap.set('n', '<F9>', builtin.diagnostics, { desc = 'search diagnostics' })
       vim.keymap.set('n', '<F12>', builtin.resume, { desc = 'telescope resume' })
       -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
