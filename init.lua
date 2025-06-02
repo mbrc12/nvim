462a32f7ae9e9fc0973309894cf822fb68900cfb
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2025-06-01 20:10:07 -0700

    project rooter and some pushes

--- init.lua
+++ init.lua
@@ -32,16 +32,6 @@ vim.opt.autoread = true
 vim.opt.cmdheight = 0
 vim.opt.hlsearch = true -- Set highlight on search, but clear on pressing <Esc> in normal mode
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -197,6 +187,12 @@ require("lazy").setup({
       direction = "horizontal"
     },
   },
+  -- Lua
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+    }
+  },
 
   {
     "folke/trouble.nvim",
@@ -210,46 +206,48 @@ require("lazy").setup({
       },
     }
   },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    lazy = false,
-    config = function()
-      require('barbar').setup {
-        auto_hide = 1,
-        insert_at_end = true,
-        icons = {
-          button = '',
-        },
-      }
-    end,
+  -- {
+  --   'romgrk/barbar.nvim',
+  --   dependencies = { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  --   lazy = false,
+  --   config = function()
+  --     require('barbar').setup {
+  --       auto_hide = 1,
+  --       insert_at_end = true,
+  --       icons = {
+  --         button = '',
+  --       },
+  --     }
+  --   end,
+  --
+  --   keys = (function()
+  --     local keys = {
+  --       { '<leader>tr', '<Cmd>BufferMoveNext<CR>',     desc = 'buffer move to next' },
+  --       { '<leader>tl', '<Cmd>BufferMovePrevious<CR>', desc = 'buffer move to previous' },
+  --       { '<C-q>',      '<Cmd>BufferWipeout<CR>',      desc = 'close buffer' },
+  --     }
+  --
+  --     for i = 1, 10 do
+  --       table.insert(keys,
+  --         { '<M-' .. i .. '>', '<Cmd>BufferGoto ' .. i .. '<CR>', 'change tabs', mode = { 't', 'n' } })
+  --     end
+  --
+  --     return keys
+  --   end)(),
+  -- }, --barbar
+  --
+  -- {
+  --   "ahmedkhalf/project.nvim",
+  --   lazy = false,
+  --   dependencies = { "nvim-telescope/telescope.nvim" },
+  --   config = function()
+  --     require("project_nvim").setup {
+  --       patterns = { ".git", "latexmkrc", "go.mod", "package.json", ".root" }
+  --     }
+  --     require("telescope").load_extension("projects")
+  --   end,
+  -- },
 
-    keys = (function()
-      local keys = {
-        { '<leader>tr', '<Cmd>BufferMoveNext<CR>',     desc = 'buffer move to next' },
-        { '<leader>tl', '<Cmd>BufferMovePrevious<CR>', desc = 'buffer move to previous' },
-        { '<C-q>',      '<Cmd>BufferWipeout<CR>',      desc = 'close buffer' },
-      }
-
-      for i = 1, 10 do
-        table.insert(keys, { '<M-' .. i .. '>', '<Cmd>BufferGoto ' .. i .. '<CR>', 'change tabs', mode = { 't', 'n' } })
-      end
-
-      return keys
-    end)(),
-  }, --barbar
-
-  {
-    "ahmedkhalf/project.nvim",
-    lazy = false,
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("project_nvim").setup {
-        patterns = { ".git", "latexmkrc", "go.mod", "package.json", ".root" }
-      }
-      require("telescope").load_extension("projects")
-    end,
-  },
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = {
@@ -288,7 +286,6 @@ require("lazy").setup({
       { '<leader>d', '<cmd>NvimTreeToggle<CR>', desc = 'Nvim-tree toggle' },
     },
   },
-
 
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
@@ -341,7 +338,8 @@ require("lazy").setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = 'live grep' })
-      vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'fuzzy search in current file' })
+      vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find,
+        { desc = 'fuzzy search in current file' })
       vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'find files' })
       vim.keymap.set('n', '<F9>', builtin.diagnostics, { desc = 'search diagnostics' })
       vim.keymap.set('n', '<F12>', builtin.resume, { desc = 'telescope resume' })
@@ -416,7 +414,8 @@ require("lazy").setup({
 
           -- Make lsp popup borders rounded by replacing the function for preview
           -- and also limit the width
-          local border = 'single'
+          -- local border = 'single'
+          local border = 'rounded'
 
           local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
           function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
@@ -460,7 +459,8 @@ require("lazy").setup({
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight',
+              { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
               group = highlight_augroup,
@@ -628,6 +628,22 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+        symbols = {
+          icon_fetcher = function(kind, bufnr, symbol) return kind:sub(1, 1) end,
+        }
+      }
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
@@ -741,13 +757,15 @@ require("lazy").setup({
 
         window = {
           completion = {
-            border = 'single',
+            -- border = 'single',
+            border = 'rounded',
             -- winhighlight = "NormalFloat:Normal"
             winhighlight = 'Normal:CmpNormal',
           },
 
           documentation = {
-            border = 'single',
+            -- border = 'single',
+            border = 'rounded',
             max_width = 60,
             -- max_height = 20,
           },
@@ -909,17 +927,25 @@ require("lazy").setup({
 }, {})
 
 --- Set some keymaps
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Change window' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Change window' })
+
+local telescope_builtin = require 'telescope.builtin'
 
 local wk = require 'which-key'
 wk.add({
+  { '<Esc><Esc>', '<C-\\><C-n>',                    desc = 'Exit terminal mode',              mode = { 't' } },
   { '<Esc>',      '<cmd>nohlsearch<CR>',            desc = "remove highlights" },
-  -- Diagnostic keymaps
   { '<leader>e',  vim.diagnostic.open_float,        desc = 'Show diagnostic [E]rror messages' },
   { '<C-s>',      ":w<CR>",                         desc = 'Save' },
   { '<C-s>',      "<Esc>:w<CR>",                    desc = 'Save',                            mode = 'i' },
-  { "<leader>if", ":e ~/.config/nvim/init.lua<CR>", desc = "edit config" }
+  { "<leader>if", ":e ~/.config/nvim/init.lua<CR>", desc = "edit config" },
+  { "<M-s>",      "<esc><C-w>p",                    desc = "goto other window",               mode = { 'n', 'i' } },
+  { "<M-s>",      "<C-\\><C-n><C-w>p",              desc = "goto other window",               mode = { 't' } },
+  { "<M-w>",      telescope_builtin.buffers,        desc = "list buffers",                    mode = { 'n', 't' } },
+  { "<C-q>",      ":bw!<CR>",                       desc = "close buffer",                    mode = { 'n' } },
+  { "<C-q>",      "<C-\\><C-n>:bw!<CR>",            desc = "close buffer",                    mode = { 't' } },
+  { "<F9>",       telescope_builtin.oldfiles,       desc = "recent files" },
 })
 
 -- A custom setup to limit width via comments, primarily for latex or markdown documents.
@@ -974,6 +1000,84 @@ wk.add({
   { '<F10>', ToggleColorscheme, desc = 'Toggle night/day mode' },
 })
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'TermOpen' }, {
+  -- pattern = { 'term://*' },
+  pattern = { '*' },
+  desc = 'Enter insert in terminal immediately',
+  group = vim.api.nvim_create_augroup('personal-terminal', { clear = true }),
+  callback = function()
+    if vim.opt.buftype:get() == 'terminal' then
+      vim.cmd.startinsert()
+    end
+  end,
+})
+
+----- Project rooter ----------------
+
+local project_rooter_config = {
+  patterns = { '.git', 'CMakeLists.txt', 'Makefile', 'package.json', 'Cargo.toml', 'pyproject.toml', 'go.mod', 'main.tex', '.root' },
+  level_limit = 5, -- how many levels to go up
+}
+
+local function ProjectRooter()
+  config = project_rooter_config
+  local patterns = config.patterns
+
+  local current = vim.fn.expand('%:p:h')
+  local level = 0
+
+  local found = nil
+
+  while found == nil and level <= config.level_limit do
+    if vim.fn.isdirectory(current) == 1 then
+      for _, pattern in ipairs(patterns) do
+        if vim.fn.glob(current .. '/' .. pattern) ~= '' then
+          -- Found a project root, set the working directory
+          found = current
+          break
+        end
+      end
+    end
+
+    if found ~= nil then
+      break
+    end
+
+    current = vim.fn.fnamemodify(current, ':h')
+    level = level + 1
+  end
+
+  if found == nil then
+    -- No project root found, notify the user
+    vim.notify('No project root found in ' .. vim.fn.expand('%:p:h'), vim.log.levels.WARN)
+    return
+  end
+
+  vim.ui.input({
+    prompt = 'Root found. Confirm: ',
+    default = found,
+    completion = 'dir',
+  }, function(input)
+    if input ~= nil and vim.fn.isdirectory(input) == 1 then
+      vim.cmd.cd(input)
+    end
+  end)
+end
+
+wk.add({
+  { '<leader>pp', ProjectRooter, desc = 'Project rooter' },
+})
 
 --- SET COLORSCHEMES BEFORE THIS
 
@@ -981,13 +1085,16 @@ wk.add({
 -- vim.o.guifont = "Iosevka Extended:h14"
 -- vim.o.guifont = "CommitMono Nerd Font:h15"
 -- vim.o.guifont = "Hurmit Nerd Font:h14"
-vim.o.guifont = "JetBrains Mono:h11"
--- vim.o.guifont = "Fira Code:h12"
--- vim.o.guifont = "Hasklug Nerd Font:h16"
+-- vim.o.guifont = "JetBrains Mono:h11"
+vim.o.guifont = "Fira Code Retina:h12"
+-- vim.o.guifont = "Hasklug Nerd Font:h12"
 -- vim.o.guifont = "CaskaydiaCove Nerd Font:h12"
 -- vim.o.guifont = "IosevkaTerm Nerd Font:h12"
--- vim.o.guifont = "IosevkaTermSlab Nerd Font:h12"
--- vim.o.guifont = "UbuntuMono Nerd Font:h14.5"
+-- vim.o.guifont = "IosevkaTerm Nerd Font:h14"
+-- vim.o.guifont = "UbuntuMono Nerd Font:h15"
+-- vim.o.guifont = "Adwaita Mono:h13"
+-- vim.o.guifont = "Intel One Mono:h13.5"
+-- vim.o.guifont = "InputMono Nerd Font:h13.5"
 
 function NeovideFullscreen()
   if vim.g.neovide_fullscreen == true then
@@ -997,8 +1104,10 @@ function NeovideFullscreen()
   end
 end
 
-vim.g.neovide_scroll_animation_length = 0
-vim.g.neovide_cursor_animation_length = 0
+local animation_length = 0.02
+
+vim.g.neovide_scroll_animation_length = animation_length
+vim.g.neovide_cursor_animation_length = animation_length
 vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_cursor_vfx_particle_density = 20.0
